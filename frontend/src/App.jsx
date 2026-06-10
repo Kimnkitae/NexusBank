cbf7c66628bec32e4c4f6f8a3ea251e1f6f821ea
Admin <kimnkitae@gmail.com>
2026-06-10 18:25:56 +0300

    Добавление бэкенда

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from 'react'
 import Balance from './bank/Balance.jsx'
 import { BalanceContext } from './bank/BalanceContext.jsx'
 import Deposit from './bank/Deposit.jsx'
+import Authorization from './authorization/authorization.jsx'
 
 import './app.css'
 
@@ -14,6 +15,7 @@ function App() {
       <div className="container">
         <BalanceContext.Provider value={{ balance, setBalance}}>
 
+          <Authorization />
           <Balance />
           <Deposit />
         </BalanceContext.Provider>
--- frontend/src/authorization/authorization.jsx
+++ frontend/src/authorization/authorization.jsx
@@ -1,14 +1,65 @@
-import { React, useState } from 'React'
+import React, { useState } from 'react'
+import './authorization.css'
 
 const Authorization = () => {
+    const [name, setName] = useState('')
+    const [users, setUsers] = useState([])
+    
+
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (!name.trim()) {
+            setMessage('Введите имя!')
+            return
+        }
+
+        try {
+            const response = await fetch('http://localhost:3001/users', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name })
+            })
+
+            const data = await response.json()
+            setName('')
+            fetchUsers()
+        } catch (error) {
+            setMessage('Ошибка соединения с сервером')
+        }
+    }
+
+   
+    const fetchUsers = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/users')
+            const data = await response.json()
+            setUsers(data)
+        } catch (error) {
+            setMessage('Ошибка загрузки пользователей')
+        }
+    }
+
     return (
-        <>
-            <div className="form">
-                <form action="">
-                    <p>Enter your name</p>
-                    <input type="text" className='name' />
-                </form>
-            </div>
-        </>
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <p className='enterName'>Enter your name</p>
+                <input
+                    type="text"
+                    className="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <button type="submit">Send</button>
+            </form>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>ID: {user.id} — {user.name}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
+
+export default Authorization
